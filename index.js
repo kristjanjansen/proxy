387b7e601a2811bdb18481bffaee5bfe39756afd
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-03-29 17:05:15 +0200

    cleanup

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = new Hono({
   getPath: (req) => req.url.replace(/^https?:\/(.+?)$/, "$1"),
 });
 const browser = await puppeteer.launch({
-  executablePath: "/usr/bin/google-chrome",
+  executablePath: process.argv[3],
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
 });
 
@@ -29,14 +29,22 @@ app.get("/*", async (c) => {
 
   await page.goto(fetchUrl);
   const a = await page.content();
-  console.log(a);
   const content = await page.evaluate(() => {
-    const pre = document.querySelector("pre");
-    return pre ? pre.textContent : "{}";
+    const pres = document.querySelectorAll("pre");
+    if (pres?.length === 1) {
+      return { json: JSON.parse(pres[0].textContent) };
+    }
+    const body = document.querySelector("body");
+    return { text: body.textContent };
   });
-  await page.close(); // Close the page to free up resources.
-  const json = JSON.parse(content);
-  return c.json(json);
+  await page.close();
+  if (content.json) {
+    return c.json(content.json);
+  }
+  if (content.text) {
+    return c.text(content.text);
+  }
+  return c.text("");
 });
 
 const port = process.argv[2] || 4000;
