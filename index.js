5a5a69bf06348c150bad85e5d50fbbbc9ac63d0e
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-03-29 10:26:45 +0200

    puppeteer

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { chromium, devices } from "playwright";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+puppeteer.use(StealthPlugin());
 
 const app = new Hono({
   getPath: (req) => req.url.replace(/^https?:\/(.+?)$/, "$1"),
 });
-const browser = await chromium.launch({ headless: true });
-const chrome = devices["Desktop Chrome"];
-const context = await browser.newContext(chrome);
+const browser = await puppeteer.launch({ headless: true });
+
+// Corrected to directly use a new page for each request.
+// If incognito mode is needed, uncomment the relevant lines.
 
 app.use("/*", cors());
 
@@ -18,12 +21,17 @@ app.get("/*", async (c) => {
     return c.text("");
   }
   const fetchUrl = url.pathname.replace(/^\//, "") + url.search;
-  const page = await context.newPage();
+
+  const page = await browser.newPage();
+
   await page.goto(fetchUrl);
+  const a = await page.content();
+  console.log(a);
   const content = await page.evaluate(() => {
     const pre = document.querySelector("pre");
     return pre ? pre.textContent : "{}";
   });
+  await page.close(); // Close the page to free up resources.
   const json = JSON.parse(content);
   return c.json(json);
 });
